27e809b15a072d1baa009b42cdb995f8333790d6
Mark Borcherding <markborcherding@gmail.com>
2014-11-05 19:22:13 -0600

    level 5

--- rubywarrior/mark-beginner/player.rb
+++ rubywarrior/mark-beginner/player.rb
@@ -1,7 +1,14 @@
 class Player
+
+  MAX_HEALTH = 20
+
   def play_turn(warrior)
+    took_damage = warrior.health < (@previous_health || MAX_HEALTH)
+    puts "==> Taking damage." if took_damage
+    @previous_health = warrior.health
     return warrior.attack! unless warrior.feel.empty?
-    return warrior.rest! unless warrior.health == 20
+    return warrior.rest! if warrior.health < MAX_HEALTH && !took_damage
     warrior.walk!
   end
 end
+
