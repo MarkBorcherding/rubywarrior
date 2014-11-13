cf235fdd4b7dcbac1677c4e06871a80e07e27701
Mark Borcherding <markborcherding@gmail.com>
2014-11-12 19:52:48 -0600

    level 3

--- rubywarrior/decorator-intermediate/player.rb
+++ rubywarrior/decorator-intermediate/player.rb
@@ -4,6 +4,24 @@ module Walk
   end
 end
 
+module Attack
+  def play_turn(warrior)
+    [:forward, :left, :right, :backward].each do |direction|
+      return warrior.attack! direction if warrior.feel(direction).enemy?
+    end
+    super(warrior)
+  end
+end
+
+module Rest
+  def play_turn(warrior)
+    return warrior.rest! unless warrior.health == 20
+    super(warrior)
+  end
+end
+
 class Player
   include Walk
+  include Rest
+  include Attack
 end
